8608ae22a214bc79fda6022c779648214317659e
Gorm Lai <gorm@kotoristudios.com>
2019-03-20 19:38:47 +0000

    first version

---
+++ Palette.h
@@ -0,0 +1,51 @@
+#ifndef _PALETTE_H_
+#define _PALETTE_H_
+
+/*
+ LICENSE - this file is public domain
+ 
+ This is free and unencumbered software released into the public domain.
+ 
+ Anyone is free to copy, modify, publish, use, compile, sell, or
+ distribute this software, either in source code form or as a compiled
+ binary, for any purpose, commercial or non-commercial, and by any
+ means.
+ 
+ In jurisdictions that recognize copyright laws, the author or authors
+ of this software dedicate any and all copyright interest in the
+ software to the public domain. We make this dedication for the benefit
+ of the public at large and to the detriment of our heirs and
+ successors. We intend this dedication to be an overt act of
+ relinquishment in perpetuity of all present and future rights to this
+ software under copyright law.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ 
+ For more information, please refer to <http://unlicense.org/>
+ 
+ */
+
+class Palette
+{
+public:
+    // idea inspired by this https://iquilezles.org/www/articles/palettes/palettes.htm
+    template<typename T, unsigned int DIMENSIONS>
+    static void inigoQuilez(const T t, const T palette[4][DIMENSIONS], T result[DIMENSIONS])
+    {
+        constexpr T twopi = T(6.28318530718);
+        for(unsigned int i=0 ; i < DIMENSIONS ; i++)
+        {
+            T val = palette[0][i] + palette[1][i] * cos(twopi * (palette[2][i]*t + palette[3][i]));
+            result[i] = val;
+        }
+    };
+    
+};
+
+#endif
